0c5cb8428336acba3c6cf814e01f5bdc28eddfc0
Steveboo <steveboo@web.de>
2014-07-16 16:51:07 +0200

    adding

--- AllyStat/src/allystat/AllyStat.java
+++ AllyStat/src/allystat/AllyStat.java
@@ -6,6 +6,10 @@
 
 package allystat;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author Steveboo
@@ -16,7 +20,35 @@ public class AllyStat {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       int x;
+       int y;
+        try {
+		BufferedReader in = new BufferedReader(new FileReader("Allystats.txt"));
+		String zeile = null;
+		while ((zeile = in.readLine()) != null) {
+		                if(zeile.startsWith("Name")){}
+                                else{
+                                    if(zeile.startsWith("Ste")){
+                                    //x=zeile.codePointAt(8);
+                                   // x=zeile.indexOf(9);
+                                  //  zeile.
+                                   // zeile.la
+                                        zeile=zeile.replaceAll(" ",".");
+                                        x=zeile.indexOf(".");
+                                        y=zeile.indexOf(".", x+1);
+                                        zeile=zeile.replaceAll(zeile.substring(x, y),"");
+                                        x=zeile.indexOf("[");
+                                        y=zeile.indexOf("]");
+                                       zeile=zeile.replaceAll(zeile.substring(x, y),"");
+                System.out.println( zeile);
+                } 
+                                }
+		}
+
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+        
     }
     
 }
